6ec25618e96857e7b11dbae00381dcc1cbd10314
Fathur Wiriansyah <90176193+Wrianzz@users.noreply.github.com>
2026-03-05 15:14:10 +0700

    Update PipelineGuard.groovy

--- jenkins/lib/PipelineGuard.groovy
+++ jenkins/lib/PipelineGuard.groovy
@@ -1,17 +1,7 @@
-class PipelineGuard implements Serializable {
-  private final def steps
-
-  PipelineGuard(def steps) {
-    this.steps = steps
-  }
-
-  void requestApproval(String pipelineMode, String workflowId, String submitter) {
-    steps.input(
-      message: "Mode ${pipelineMode}: yakin lanjut promote 1 file workflow (workflow/${workflowId}) ke master + deploy ke PROD?",
-      ok: 'Approve & Continue',
-      submitter: submitter
-    )
-  }
+def requestApproval(String pipelineMode, String workflowId, String submitter) {
+  input(
+    message: "Mode ${pipelineMode}: yakin lanjut promote 1 file workflow (workflow/${workflowId}) ke master + deploy ke PROD?",
+    ok: 'Approve & Continue',
+    submitter: submitter
+  )
 }
-
-return new PipelineGuard(this)
